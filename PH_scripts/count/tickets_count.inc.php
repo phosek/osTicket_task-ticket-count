f65b1275864e8581203189c7b0207935d31c01de
Pavel Hošek <115976233+phosek@users.noreply.github.com>
2023-07-26 14:30:20 +0200

    is_null
    Added is_null check

--- PH_scripts/count/tickets_count.inc.php
+++ PH_scripts/count/tickets_count.inc.php
@@ -1,20 +1,23 @@
 <?php
 $ph_count_tickets = 0;
-$ph_staff_id = $thisstaff->getId();
-$spojeni = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
-$ost_dept = mysqli_query($spojeni, "SELECT ost_staff.dept_id FROM ost_staff WHERE ost_staff.staff_id = $ph_staff_id");
-	while ($zaznam_ticket = mysqli_fetch_array ($ost_dept)) 
-	{
-		$ph_dept_id = $zaznam_ticket["dept_id"];
-		$ost_tcnt = mysqli_query($spojeni, "SELECT COUNT(`ticket_id`) AS count FROM `ost_ticket` WHERE `status_id` = 1 AND `dept_id` = $ph_dept_id");
-		while ($zaznam_tcnt = mysqli_fetch_array ($ost_tcnt)) {$ph_count_tickets = $ph_count_tickets + $zaznam_tcnt["count"];}
-	}		
-$ost_dept = mysqli_query($spojeni, "SELECT ost_staff_dept_access.dept_id FROM ost_staff_dept_access WHERE ost_staff_dept_access.staff_id = $ph_staff_id");
-	while ($zaznam_ticket = mysqli_fetch_array ($ost_dept)) 
-	{
-		$ph_dept_id = $zaznam_ticket["dept_id"];
-		$ost_tcnt = mysqli_query($spojeni, "SELECT COUNT(`ticket_id`) AS count FROM `ost_ticket` WHERE `status_id` = 1 AND `dept_id` = $ph_dept_id");
-		while ($zaznam_tcnt = mysqli_fetch_array ($ost_tcnt)) {$ph_count_tickets = $ph_count_tickets + $zaznam_tcnt["count"];}
-	}
-define('ph_count_tickets', $ph_count_tickets)
+if (!is_null($thisstaff->getId()))
+{
+	$ph_staff_id = $thisstaff->getId();
+	$spojeni = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME);
+	$ost_dept = mysqli_query($spojeni, "SELECT ost_staff.dept_id FROM ost_staff WHERE ost_staff.staff_id = $ph_staff_id");
+		while ($zaznam_ticket = mysqli_fetch_array ($ost_dept)) 
+		{
+			$ph_dept_id = $zaznam_ticket["dept_id"];
+			$ost_tcnt = mysqli_query($spojeni, "SELECT COUNT(`ticket_id`) AS count FROM `ost_ticket` WHERE `status_id` = 1 AND `dept_id` = $ph_dept_id");
+			while ($zaznam_tcnt = mysqli_fetch_array ($ost_tcnt)) {$ph_count_tickets = $ph_count_tickets + $zaznam_tcnt["count"];}
+		}		
+	$ost_dept = mysqli_query($spojeni, "SELECT ost_staff_dept_access.dept_id FROM ost_staff_dept_access WHERE ost_staff_dept_access.staff_id = $ph_staff_id");
+		while ($zaznam_ticket = mysqli_fetch_array ($ost_dept)) 
+		{
+			$ph_dept_id = $zaznam_ticket["dept_id"];
+			$ost_tcnt = mysqli_query($spojeni, "SELECT COUNT(`ticket_id`) AS count FROM `ost_ticket` WHERE `status_id` = 1 AND `dept_id` = $ph_dept_id");
+			while ($zaznam_tcnt = mysqli_fetch_array ($ost_tcnt)) {$ph_count_tickets = $ph_count_tickets + $zaznam_tcnt["count"];}
+		}
+}
+define('ph_count_tickets', $ph_count_tickets);
 ?>
